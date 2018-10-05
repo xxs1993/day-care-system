ef21b9f3463ddecd8de126766a8e0b1d1dbcb91f
xxs1993 <42723291+xxs1993@users.noreply.github.com>
2018-10-05 01:23:17 -0400

    文件读入读出

---
+++ src/test/csye6200/util/FileUtilTest.java
@@ -0,0 +1,112 @@
+package test.csye6200.util;
+
+
+import com.google.common.collect.Lists;
+import csye6200.util.FileUtil;
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import java.util.List;
+
+
+/**
+* FileUtil Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>10/05/2018</pre> 
+* @version 1.0 
+*/ 
+public class FileUtilTest extends TestCase {
+public FileUtilTest(String name) { 
+super(name); 
+} 
+
+public void setUp() throws Exception { 
+super.setUp(); 
+} 
+
+public void tearDown() throws Exception { 
+super.tearDown(); 
+} 
+
+/** 
+* 
+* Method: isFileWriting(String fileName) 
+* 
+*/
+@Test
+public void testIsFileWriting() throws Exception { 
+//TODO: Test goes here... .
+
+
+}
+
+/** 
+* 
+* Method: writeToFile(String fileName, List<String> contents) 
+* 
+*/ 
+public void testWriteToFile() throws Exception { 
+//TODO: Test goes here...
+    List<String> list = Lists.newArrayList();
+    for(int i=0;i<20;i++){
+        list.add("qq,213,1313,13");
+        list.add("sds,dsd,ddd0,ccc");
+    }
+    FileUtil.writeToFile("student.csv",list);
+
+} 
+
+/** 
+* 
+* Method: readContents(String fileName) 
+* 
+*/ 
+public void testReadContents() throws Exception { 
+//TODO: Test goes here...
+    List<String> contents = FileUtil.readContents("student.csv");
+    System.out.println(contents.stream().count());
+} 
+
+
+/** 
+* 
+* Method: init() 
+* 
+*/ 
+public void testInit() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = FileUtil.getClass().getMethod("init"); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+/** 
+* 
+* Method: addFlag(String fileName) 
+* 
+*/ 
+public void testAddFlag() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = FileUtil.getClass().getMethod("addFlag", String.class); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+
+
+} 
