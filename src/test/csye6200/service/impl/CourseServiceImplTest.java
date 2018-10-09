71b3b24c4697de8933ef4c1d3d1a998219442fce
xxs1993 <42723291+xxs1993@users.noreply.github.com>
2018-10-09 15:30:41 -0400

    add test file

---
+++ src/test/csye6200/service/impl/CourseServiceImplTest.java
@@ -0,0 +1,145 @@
+package test.csye6200.service.impl; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* CourseServiceImpl Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Ê®ÔÂ 9, 2018</pre> 
+* @version 1.0 
+*/ 
+public class CourseServiceImplTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: getCourses() 
+* 
+*/ 
+@Test
+public void testGetCourses() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getCourseById(String id) 
+* 
+*/ 
+@Test
+public void testGetCourseById() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getStudentsIdByCourseId(String id) 
+* 
+*/ 
+@Test
+public void testGetStudentsIdByCourseId() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getStudentsInCourse(String id) 
+* 
+*/ 
+@Test
+public void testGetStudentsInCourse() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getTeachersIdByCourseId(String id) 
+* 
+*/ 
+@Test
+public void testGetTeachersIdByCourseId() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getTeachersInCourse(String id) 
+* 
+*/ 
+@Test
+public void testGetTeachersInCourse() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: addCourse(Course course) 
+* 
+*/ 
+@Test
+public void testAddCourse() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: removeCourse(int id) 
+* 
+*/ 
+@Test
+public void testRemoveCourse() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: addStudents(List<Student> students) 
+* 
+*/ 
+@Test
+public void testAddStudents() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: addTeachers(List<Teacher> teachers) 
+* 
+*/ 
+@Test
+public void testAddTeachers() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: removeTeachers(List<Integer> list) 
+* 
+*/ 
+@Test
+public void testRemoveTeachers() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: removeStudents(List<Integer> list) 
+* 
+*/ 
+@Test
+public void testRemoveStudents() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
