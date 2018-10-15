51b3bbd5e8c28fde3fda53388afe263a0fc6e98f
AlexYiMu <42689860+AlexYiMu@users.noreply.github.com>
2018-10-14 21:38:56 -0400

    Add files via upload

--- src/csye6200/service/TeacherService.java
+++ src/csye6200/service/TeacherService.java
@@ -4,20 +4,14 @@ import java.util.List;
 
 import csye6200.entity.ClassRoom;
 import csye6200.entity.Course;
+import csye6200.entity.Student;
 import csye6200.entity.Teacher;
 
 public interface TeacherService {
-	 	List<Teacher> getTeacher();
-	    Teacher getTeacherByID(String id);
-	    List<Course> getCourse();
-	    List<ClassRoom>getClassroom();
-	    List<Integer> getCredits();
-	    List<String> getId();
-	    
-	    boolean addTeacher(Teacher teacher);
-	    boolean removeTeacher(String id);
-	    boolean addCourse(List<Course> courses);
-	    boolean removeCourse(List<Integer> list);
-	    boolean addClassroom(List<ClassRoom>classrooms);
-	    boolean removeClassroom(List<Integer>list);
+	List<Teacher> getTeacher();
+    String addTeacher(Teacher teacher);
+    List<Student> addStudent(String id);
+    List<Student> getStudent(String id);
+    boolean deleteTeacher();
+    boolean deleteStudent(List<String> idList);
 }
